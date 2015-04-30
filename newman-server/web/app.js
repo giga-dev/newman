e1944bd4dee884fcea9ed0287429e2957bb4916e
Barak Bar Orion <barak.bar@gmail.com>
2015-04-30 21:41:34 +0300

    Personalized.

--- newman-server/web/app.js
+++ newman-server/web/app.js
@@ -1,3 +1,7 @@
 var app = document.querySelector("#app");
 app.heading="Hello Newman users!";
 app.selected=0;
+app.ajaxResponse = function(event, object){
+    app.user = object.response;
+    app.heading = "Hello " + app.user.userName;
+}
