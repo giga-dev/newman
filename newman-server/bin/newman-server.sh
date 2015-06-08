d88398d8aeafcbe646af576105e38b528f2d7a01
mavigdor <moran@gigaspaces.com>
2015-06-08 16:42:22 +0300

    add realm.properties if it doesn't exist

--- newman-server/bin/newman-server.sh
+++ newman-server/bin/newman-server.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# make sure keys have been generated, if not create for development
 if [ ! -d "../keys" ]; then
   ./keysgen.sh
 fi
 
+# make sure realm properties exist, if not create a for development
+mkdir -p ../config
+if [ ! -a "../config/realm.properties" ]; then
+   echo "root: root, admin" >> ../config/realm.properties
+fi
+
+# run newman server
 java -Dnewman.server.realm-config-path=../config/realm.properties -Dnewman.keys-folder-path=../keys/server.keystore -Dnewman.server.web-folder-path=../web -jar ../target/newman-server-1.0.jar
