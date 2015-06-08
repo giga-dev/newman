76f7d13f31ea30c953df2ebfece925c894c76269
mavigdor <moran@gigaspaces.com>
2015-06-08 16:30:17 +0300

    generate keys first time

--- newman-server/bin/newman-server.sh
+++ newman-server/bin/newman-server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+if [ ! -d "../keys" ]; then
+  ./keysgen.sh
+fi
+
 java -Dnewman.server.realm-config-path=../config/realm.properties -Dnewman.keys-folder-path=../keys/server.keystore -Dnewman.server.web-folder-path=../web -jar ../target/newman-server-1.0.jar
