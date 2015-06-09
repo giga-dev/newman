fd1054d4197d76dbdcaa34622926572a1a0a9c35
mavigdor <moran@gigaspaces.com>
2015-06-09 12:32:06 +0300

    add newman.mongo.db.name override

--- newman-agent/bin/newman-agent.sh
+++ newman-agent/bin/newman-agent.sh
@@ -8,5 +8,9 @@ NEWMAN_PASSWORD=
 NEWMAN_SERVER_HOST=
 # default is 8443
 NEWMAN_SERVER_PORT=
+# db name default is account user name who started newman server
+if [ -z "${NEWMAN_DB_NAME}" ]; then
+    NEWMAN_DB_NAME=${USER}
+fi
 
-java -jar ../target/newman-agent-1.0.jar -Dnewman.agent.home=../ -Dnewman.agent.hostname=`hostname`  -Dnewman.agent.server-host=${NEWMAN_SERVER_HOST} -Dnewman.agent.server-port=${NEWMAN_SERVER_PORT} -Dnewman.agent.server-rest-user=${NEWMAN_USERNAME} -Dnewman.agent.server-rest-pw=${NEWMAN_PASSWORD}
+java -jar ../target/newman-agent-1.0.jar -Dnewman.mongo.db.name=${NEWMAN_DB_NAME} -Dnewman.agent.home=../ -Dnewman.agent.hostname=`hostname`  -Dnewman.agent.server-host=${NEWMAN_SERVER_HOST} -Dnewman.agent.server-port=${NEWMAN_SERVER_PORT} -Dnewman.agent.server-rest-user=${NEWMAN_USERNAME} -Dnewman.agent.server-rest-pw=${NEWMAN_PASSWORD}
