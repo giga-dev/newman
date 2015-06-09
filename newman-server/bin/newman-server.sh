5c866774d875501fffd3135bfeea21059763aa68
moran <moran@gigaspaces.com>
2015-06-09 12:55:21 +0300

    add newman.mongo.db.host and newman.mongo.db.name as sys props in script

--- newman-server/bin/newman-server.sh
+++ newman-server/bin/newman-server.sh
@@ -11,5 +11,10 @@ if [ ! -a "../config/realm.properties" ]; then
    echo "root: root, admin" >> ../config/realm.properties
 fi
 
+#default mongo host is localhost
+NEWMAN_MONGO_DB_HOST=
+#default mongo db name is account user name defined by ${USER}
+NEWMAN_MONGO_DB_NAME=
+
 # run newman server
-java -Dnewman.server.realm-config-path=../config/realm.properties -Dnewman.keys-folder-path=../keys/server.keystore -Dnewman.server.web-folder-path=../web -jar ../target/newman-server-1.0.jar mongo-config.json
+java -Dnewman.mongo.db.host=${NEWMAN_MONGO_DB_HOST} -Dnewman.mongo.db.name=${NEWMAN_MONGO_DB_NAME} -Dnewman.server.realm-config-path=../config/realm.properties -Dnewman.keys-folder-path=../keys/server.keystore -Dnewman.server.web-folder-path=../web -jar ../target/newman-server-1.0.jar
