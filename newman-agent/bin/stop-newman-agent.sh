9f662f1f64add45189cf6741d28659b7c1f9b577
boris <boris@gigaspaces.com>
2015-07-08 15:18:50 +0300

    added stop script for agent

---
+++ newman-agent/bin/stop-newman-agent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PID=`ps -eaf | grep newman-agent | grep -v grep | awk '{print $2}'`
+echo "current pid of newman agent is: ${PID}"
+if [[ "" !=  "$PID" ]]; then
+  echo "killing newman agent with pid: ${PID}"
+  kill -9 ${PID}
+fi
