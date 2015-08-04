f8a4fc091f78c18fdf4ecbffda9f9f89786c53f4
Barak Bar Orion <barak.bar@gmail.com>
2015-08-04 14:47:01 +0300

    NMN-17 Add log size to header panel.

--- newman-server/web/app.js
+++ newman-server/web/app.js
@@ -27,6 +27,22 @@ window.app.update = function(from, to){
     return to;
 }
 
+window.app.humanFileSize = function (bytes, si) {
+            var thresh = si ? 1000 : 1024;
+            if(Math.abs(bytes) < thresh) {
+                return bytes + ' B';
+            }
+            var units = si
+                ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+                : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+            var u = -1;
+            do {
+                bytes /= thresh;
+                ++u;
+            } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+         return bytes.toFixed(1)+' '+units[u];
+}
+
 
 window.addEventListener('polymer-ready', function(e) {
 
