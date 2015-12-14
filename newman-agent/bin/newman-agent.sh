4937c28fcc54fedc2f84b0c7cbfabe4522c2a413
moran <moran@gigaspaces.com>
2015-12-14 17:56:30 +0200

    revert script default to 3 (although script is not being used - see new script at ansible folder)

--- newman-agent/bin/newman-agent.sh
+++ newman-agent/bin/newman-agent.sh
@@ -24,7 +24,7 @@ NEWMAN_AGENT_CAPABILITIES="DOCKER,LINUX"
 
 # newman agent workers, if not set default to 1
 if [ -z "${NEWMAN_AGENT_WORKERS}" ]; then
-   NEWMAN_AGENT_WORKERS=1
+   NEWMAN_AGENT_WORKERS=3
 fi
 
 nohup java -Dnewman.agent.workers=${NEWMAN_AGENT_WORKERS} -Dnewman.agent.home=${NEMAN_AGENT_HOME} -Dnewman.agent.hostname=`hostname`  -Dnewman.agent.server-host=${NEWMAN_SERVER_HOST} -Dnewman.agent.server-port=${NEWMAN_SERVER_PORT} -Dnewman.agent.server-rest-user=${NEWMAN_USERNAME} -Dnewman.agent.server-rest-pw=${NEWMAN_PASSWORD} -Dnewman.agent.capabilities=${NEWMAN_AGENT_CAPABILITIES} -jar newman-agent-1.0.jar > "nohup_agent1.out" &
