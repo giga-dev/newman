88411986e6d64e928c67bfae65d919b146d4e4b0
moran <moran@gigaspaces.com>
2015-12-14 17:55:09 +0200

    Suite requirements,custom variables, etc are being nulled. Remove usage of Suite as it affects subscribe logic.

--- newman-common/src/main/java/com/gigaspaces/newman/beans/SuiteWithJobs.java
+++ newman-common/src/main/java/com/gigaspaces/newman/beans/SuiteWithJobs.java
@@ -15,8 +15,6 @@ public class SuiteWithJobs {
     @Id
     private String id;
 
-    private Suite suite;
-
     private String name;
 
     private List<JobView> jobs = new ArrayList<JobView>();
@@ -29,17 +27,10 @@ public class SuiteWithJobs {
     }
 
     public SuiteWithJobs(Suite suite, List<Job> jobsList) {
-        this.suite = suite;
         this.jobs = new ArrayList<>( jobsList.size() );
         for( Job job : jobsList ){
             jobs.add( new JobView( job ) );
         }
-        //we don't have to display following properties them we set null values in order to decrease passed json size
-        suite.setCriteria(null);
-        suite.setCustomVariables(null);
-        suite.setRequirements(null);
-        suite.setDisplayedCriteria(null);
-
         setId( suite.getId() );
         setName( suite.getName() );
     }
@@ -66,14 +57,6 @@ public class SuiteWithJobs {
 
     public void setJobs(List<JobView> jobs) {
         this.jobs = jobs;
-    }
-
-    public Suite getSuite() {
-        return suite;
-    }
-
-    public void setSuite(Suite suite) {
-        this.suite = suite;
     }
 
     @Override
