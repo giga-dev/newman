1b401a5482c327f10d811e1cbdd3181bec20b718
Tamir Schwarz <tamirs2005@gmail.com>
2016-04-07 11:15:40 +0300

    enable run command on a specific module by export cmdToDo=".."

--- ansible/docker/run.sh
+++ ansible/docker/run.sh
@@ -9,6 +9,6 @@ skipTags=${skipTags=cron,local}
 module=${module=all}
 dns=${dns=192.168.10.11}
 
-echo "Upgrading newman environment modules: ${module}, will skip ${skipTags} tags, with dns: ${dns}..."
+echo "Upgrading newman environment modules: ${module}, will skip ${skipTags} tags, with dns: ${dns}, with cmdToDo: ${cmdToDo}..."
 #docker run -v `pwd`/../..:/newman --env pass1=$1 --env pass2=$2  -it newman/anisble
-docker run --dns=${dns} --env pass1=$1 --env pass2=$2 --env skipTags=${skipTags} --env module=${module} -it newman/anisble /bin/bash -c "cd /newman && git pull && /newman/ansible/docker/upgrade-all.sh"
+docker run --dns=${dns} --env pass1=$1 --env pass2=$2 --env skipTags=${skipTags} --env module=${module} --env cmdToDo=${cmdToDo} -it newman/anisble /bin/bash -c "cd /newman && git pull && /newman/ansible/docker/upgrade-all.sh"
--- ansible/docker/upgrade-all.sh
+++ ansible/docker/upgrade-all.sh
@@ -32,24 +32,46 @@ then
   ansible-playbook site.yml --skip-tags "${skipTags}" -i hosts -u xap
 elif [ "$module" == "agents" ]
 then
-  echo "deploying only agents"
-  ansible-playbook agents_deploy.yml --skip-tags "${skipTags}" -i hosts -u xap
+    if [ -z "$cmdToDo" ]
+    then
+    	echo "deploying only agents"
+    	ansible-playbook agents_deploy.yml --skip-tags "${skipTags}" -i hosts -u xap
+    else
+    	echo "executing $cmdToDo on $module"
+    	ansible newmanDockerAgents -s -m shell -a "$cmdToDo" -i hosts -u xap
+    fi
 elif [ "$module" == "server" ]
 then
-  echo "deploying only server"
-  ansible-playbook server_deploy.yml --skip-tags "${skipTags}" -i hosts -u xap
+    if [ -z "$cmdToDo" ]
+    then
+        echo "deploying only server"
+        ansible-playbook server_deploy.yml --skip-tags "${skipTags}" -i hosts -u xap
+    else
+        echo "executing $cmdToDo on $module"
+        ansible newmanServer -m shell -a "$cmdToDo" -i hosts -u xap
+    fi
 elif [ "$module" == "submitter" ]
 then
-  echo "deploying only submitter"
-  ansible-playbook submitter_deploy.yml --skip-tags "${skipTags}" -i hosts -u xap
+    if [ -z "$cmdToDo" ]
+    then
+        echo "deploying only submitter"
+        ansible-playbook submitter_deploy.yml --skip-tags "${skipTags}" -i hosts -u xap
+    else
+        echo "executing $cmdToDo on $module"
+        ansible newmanSubmitter -m shell -a "$cmdToDo" -i hosts -u xap
+    fi
 elif [ "$module" == "analytics" ]
 then
-  echo "deploying only analytics"
-  ansible-playbook analytics_deploy.yml --skip-tags "${skipTags}" -i hosts -u xap
+    if [ -z "$cmdToDo" ]
+    then
+        echo "deploying only analytics"
+        ansible-playbook analytics_deploy.yml --skip-tags "${skipTags}" -i hosts -u xap
+    else
+        echo "executing $cmdToDo on $module"
+        ansible newmanAnalytics -m shell -a "$cmdToDo" -i hosts -u xap
+    fi
 else
   echo "invalid module option, please choose module from the following set: {all/agents/server/submitter/analytics}"
   exit 1
 fi
-
-
 
