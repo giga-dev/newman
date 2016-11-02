70cd00a3cc56c67b5dbde2ff0c889a624b7907bb
boris <boris@gigaspaces.com>
2016-11-02 09:53:32 +0200

    added script to run command on all newman agents hosts

---
+++ ansible/docker/run-command-on-agents.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
+  then
+    echo "Usage: ./run-command-on-agents.sh <SHELL_COMMAND> <FIRST_PASSWORD> <SECOND_PASSWORD>"
+    exit 1
+fi
+export module=agents
+export cmdToDo=$1
+./run.sh $2 $3
