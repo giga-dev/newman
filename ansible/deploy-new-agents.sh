2c1a1272995d7561a5ac09f672348bf46701484a
Boaz <boaz@gigaspaces.com>
2016-11-28 11:08:45 +0200

    sudo tty mode disabled via /etc/sudoers file

--- ansible/deploy-new-agents.sh
+++ ansible/deploy-new-agents.sh
@@ -10,7 +10,7 @@ read -r -p "Did you modify the password in group_vars/newNewmanDockerAgents? [y/
 case $response in
     [yY][eE][sS]|[yY])
         sudo sed -i "s/#host_key_checking = False/host_key_checking = False/g" /etc/ansible/ansible.cfg
-        ansible-playbook new_agents_deployTEST.yml -i hosts -u xap --private-key ${PEM_LOCATION}
+        ansible-playbook new_agents_deploy.yml -i hosts -u xap --private-key ${PEM_LOCATION}
         ;;
     *)
         echo "Please modifiy the password in group_vars/newNewmanDockerAgents and re-run the script"
