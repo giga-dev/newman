c8372574a15e0c444f25c6a05fb1c1f86131b0ba
tamirt <tamirt@gigaspaces,com>
2017-01-11 17:42:24 +0200

    Reverted commented Cron job that restart newman server.

--- ansible/docker/run-server-only.sh
+++ ansible/docker/run-server-only.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 export module=server
-# password for user xap
-export ANSIBLE_PASS_1=PASSWORD_HERE
-# password for user tgrid
-export ANSIBLE_PASS_2=PASSWORD_HERE
-./run.sh $ANSIBLE_PASS_1 $ANSIBLE_PASS_2
+./run.sh $1 $2
