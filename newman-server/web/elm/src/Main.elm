885b33aa0c0e4ac866b9c2911d495a3518309344
Yohana Khoury <yohana@yohanakhoury.com>
2018-01-28 17:09:27 +0200

    added types

--- newman-server/web/elm/src/Main.elm
+++ newman-server/web/elm/src/Main.elm
@@ -1,60 +1,92 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
-main = Html.beginnerProgram
-        { model = init
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
         , view = view
         , update = update
+        , subscriptions = \_ -> Sub.none
         }
 
-init = {}
-update model = model
+
+type alias Model =
+    {}
+
+
+type Msg
+    = Msg
+
+
+init : (Model, Cmd Msg)
+init =
+    (Model , Cmd.none)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+topNavBar : Html Msg
+topNavBar =
+    div [ class "navbar-header" ]
+        [ button [ class "navbar-toggle", attribute "data-target" ".navbar-ex1-collapse", attribute "data-toggle" "collapse", type_ "button" ]
+            [ span [ class "icon-bar" ]
+                []
+            , span [ class "icon-bar" ]
+                []
+            , span [ class "icon-bar" ]
+                []
+            ]
+        , a [ class "navbar-brand", href "index" ]
+            [ text "Newman" ]
+        ]
+
+leftNavBar : Html Msg
+leftNavBar =
+    div [ class "collapse navbar-collapse navbar-ex1-collapse" ]
+        [ ul [ class "nav navbar-nav side-nav" ]
+            [ li [ class "active" ]
+                [ a [ href "first" ]
+                    [ text "First Page" ]
+                ]
+            , li [ class "" ]
+                [ a [ href "second" ]
+                    [ text "Second Page" ]
+                ]
+            , li [ class "" ]
+                [ a [ href "admin" ]
+                    [ text "admin" ]
+                ]
+            ]
+        ]
+
+bodyWrapper :  Html Msg -> Html Msg
+bodyWrapper inner =
+    div [ id "page-wrapper" ]
+        [ div [ class "container-fluid" ]
+            [ div [ class "row" ]
+                [ div [ class "col-lg-12" ]
+                    [ h1 [ class "page-header" ]
+                        [ text "Header here" ]
+                    ]
+                ]
+            , div [ class "row" ]
+                [ inner
+                ]
+            ]
+        ]
+
+view : Model -> Html Msg
 view model =
     div [ id "wrapper" ]
         [ nav [ class "navbar navbar-inverse navbar-fixed-top", attribute "role" "navigation" ]
-            [ div [ class "navbar-header" ]
-                [ button [ class "navbar-toggle", attribute "data-target" ".navbar-ex1-collapse", attribute "data-toggle" "collapse", type_ "button" ]
-                    [ span [ class "icon-bar" ]
-                        []
-                    , span [ class "icon-bar" ]
-                        []
-                    , span [ class "icon-bar" ]
-                        []
-                    ]
-                , a [ class "navbar-brand", href "index" ]
-                    [ text "Newman" ]
-                ]
-            , text "        "
-            , div [ class "collapse navbar-collapse navbar-ex1-collapse" ]
-                [ ul [ class "nav navbar-nav side-nav" ]
-                    [ li [ class "active" ]
-                        [ a [ href "first" ]
-                            [ text "First Page" ]
-                        ]
-                    , li [ class "" ]
-                        [ a [ href "second" ]
-                            [ text "Second Page" ]
-                        ]
-                    , li [ class "" ]
-                        [ a [ href "admin" ]
-                            [ text "admin                    " ]
-                        ]
-                    ]
-                ]
-            , text "    "
+            [ topNavBar
+            , leftNavBar
             ]
-        , div [ id "page-wrapper" ]
-            [ div [ class "container-fluid" ]
-                [ div [ class "row" ]
-                    [ div [ class "col-lg-12" ]
-                        [ h1 [ class "page-header" ]
-                            [ text "Header here                    " ]
-                        ]
-                    ]
-                , text "        "
-                ]
-            , text "    "
-            ]
-        , text ""
+        , bodyWrapper (text "Hello there 2")
         ]
