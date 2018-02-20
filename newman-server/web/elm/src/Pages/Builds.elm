66f28b1d2e18babd47353e676d5d66567af89249
yaeln-pcu <yaeln@gigaspaces.com>
2018-02-20 16:55:26 +0200

    added filter query for tag

--- newman-server/web/elm/src/Pages/Builds.elm
+++ newman-server/web/elm/src/Pages/Builds.elm
@@ -249,6 +249,7 @@ filterQuery query build =
             || String.startsWith query build.id
             || String.startsWith query build.name
             || String.startsWith query build.branch
+            || (List.member True (List.map (String.startsWith query) build.tags))
     then
         True
     else
