ff7e0475d25a09c896a8fc3dac6ce8b676468865
rotemhe <rotemh@gigaspaces.com>
2018-07-10 14:36:57 +0300

    Add compare builds module in Builds page.

--- newman-server/web/elm/src/Views/CompareBuilds.elm
+++ newman-server/web/elm/src/Views/CompareBuilds.elm
@@ -2,8 +2,11 @@ module Views.CompareBuilds exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 import Utils.Types exposing (..)
 import Bootstrap.Form.Select as Select
+import Bootstrap.Button as Button
+import Bootstrap.Modal as Modal exposing (..)
 
 
 type alias Model =
@@ -11,19 +14,23 @@ type alias Model =
     , oldBuild : String
     , oldBuildTime : Maybe Int
     , newBuild : String
-    , newSelected : Bool
+    , oldSelected : Bool
     , newerBuilds : Builds
+    , confirmationState : Modal.State
     }
 
 
 type Msg
     = UpdateOldBuild String
     | UpdateNewBuild String
+    | ClickCompareBuilds
+    | AcknowledgeDialog
+    | ModalMsg Modal.State
 
 
 init : Builds -> Model
 init builds =
-    Model builds "" Nothing "" False builds
+    Model builds "" Nothing "" False builds Modal.hiddenState
 
 
 view : Model -> Html Msg
@@ -43,7 +50,7 @@ view model =
             Select.item [ value data.id, selected <| model.newBuild == data.id ] [ text <| data.name ++ " (" ++ data.branch ++ ")" ]
 
         newBuildSelect = div []
-                             [ Select.select [ Select.disabled (not model.newSelected), Select.onChange UpdateNewBuild, Select.attrs [ style [ ( "width", "400px" ) ]]]
+                             [ Select.select [ Select.disabled (not model.oldSelected), Select.onChange UpdateNewBuild, Select.attrs [ style [ ( "width", "400px" ) ]]]
                                                   ([ Select.item [ value "1" ] [ text "Select New Build" ]]
                                                         ++ List.map chooseNewBuild model.newerBuilds
                                                   )]
@@ -52,7 +59,9 @@ view model =
             [ div [ class "form-inline" ]
                 [ oldBuildSelect
                 , newBuildSelect
+                , div [] [ Button.button [ Button.primary, Button.onClick ClickCompareBuilds] [ text "Compare Changes" ] ]
                 ]
+            , viewDialog model ModalMsg AcknowledgeDialog
             ]
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -62,20 +71,37 @@ update msg model =
             let
                 printOldBuildSelected = Debug.log "UpdateOldBuild" buildId
             in
-            ( { model | oldBuild = buildId
-                        , oldBuildTime = (getOldBuildTime model buildId)
-                        , newSelected = True
-                        , newerBuilds = (onlyNewerBuilds model buildId) } , Cmd.none)
+            case (buildId == "1") of
+                True ->
+                        ({ model | oldBuild = "" , newBuild = "", oldSelected = False } , Cmd.none)
+                False ->
+                        ( { model | oldBuild = buildId
+                                    , oldBuildTime = (getOldBuildTime model buildId)
+                                    , oldSelected = True
+                                    , newerBuilds = (onlyNewerBuilds model buildId) } , Cmd.none)
 
         UpdateNewBuild buildId ->
             let
                 printNewBuildSelected = Debug.log "UpdateNewBuild" buildId
             in
-            ( { model | newBuild = buildId } , Cmd.none )
+            case (buildId == "1") of
+                True ->
+                    ( { model | newBuild = "" } , Cmd.none )
+                False ->
+                    ( { model | newBuild = buildId } , Cmd.none )
+
+        ClickCompareBuilds ->
+                ( { model | confirmationState = Modal.visibleState } , Cmd.none )
+
+        AcknowledgeDialog ->
+                ( { model | confirmationState = Modal.hiddenState } , Cmd.none )
+
+        ModalMsg newState ->
+                ( { model | confirmationState = newState } , Cmd.none )
 
 
 
-
+-- With a given buildId returns buildTime from model.allBuilds --
 getOldBuildTime : Model -> String -> Maybe Int
 getOldBuildTime model buildId =
                      model.allBuilds
@@ -84,6 +110,7 @@ getOldBuildTime model buildId =
                             |> List.head
 
 
+-- With a given buildId returns returns only builds which have buildTime greater or equal to given build --
 onlyNewerBuilds : Model -> String -> List Build
 onlyNewerBuilds model buildId =
           case (getOldBuildTime model buildId) of
@@ -93,8 +120,54 @@ onlyNewerBuilds model buildId =
                     model.allBuilds
 
 
+viewDialog : Model -> (State -> toMsg) -> toMsg -> Html toMsg
+viewDialog model toMsg confirmMsg =
+    case model.oldBuild of
+        "" ->
+            Modal.config toMsg
+                |> Modal.large
+                |> Modal.h3 [] [ text "Please choose an old build and try again" ]
+                |> Modal.footer []
+                    [ Button.button
+                        [ Button.outlinePrimary
+                        , Button.onClick <| toMsg Modal.hiddenState
+                        ]
+                        [ text "Close" ]
+                    ]
+                |> Modal.view model.confirmationState
+        oldBuild -> case model.newBuild of
+                        "" ->
+                            Modal.config toMsg
+                                |> Modal.large
+                                |> Modal.h3 [] [ text "Please choose a new build and try again" ]
+                                |> Modal.footer []
+                                    [ Button.button
+                                        [ Button.outlinePrimary
+                                        , Button.onClick <| toMsg Modal.hiddenState
+                                        ]
+                                        [ text "Close" ]
+                                    ]
+                                |> Modal.view model.confirmationState
+                        newBuild ->
+                            Modal.config toMsg
+                                |> Modal.large
+                                |> Modal.h2 [] [ text ("Compare builds " ++ model.oldBuild ++ " and " ++ model.newBuild) ]
+                                |> Modal.body [] [ p [] [ text "to be continued ... " ] ]
+                                |> Modal.footer []
+                                    [ Button.button
+                                        [ Button.outlinePrimary
+                                        , Button.onClick <| toMsg Modal.hiddenState
+                                        ]
+                                        [ text "Close" ]
+                                    ]
+                                |> Modal.view model.confirmationState
+
+
+
+
 
 ----------------- Links examples
+
 --}
 --li [class "nav-item" ]
 --[ a [class "nav-link", href "#" ]
