21722fb9342e276095ffd370fe1801acba42fc97
ayelet <ayelet.morris@gigaspaces.com>
2018-07-16 16:46:19 +0300

    Fix Java version issue

--- newman-common/src/main/java/com/gigaspaces/newman/beans/JavaVersion.java
+++ newman-common/src/main/java/com/gigaspaces/newman/beans/JavaVersion.java
@@ -1,14 +1,12 @@
 package com.gigaspaces.newman.beans;
 
 public enum JavaVersion {
-    ORACLE_7_21("oracle_7_21","oracle 1.0.7.21"), ORACLE_8_45("oracle_8_45","oracle 1.8.0.45"), ORACLE_9_0_4("oracle_9_0_4","oracle 9.0.4");
+    ORACLE_7_21("oracle_7_21"), ORACLE_8_45("oracle_8_45"), ORACLE_9_0_4("oracle_9_0_4");
 
     private String name;
-    private String fullVersion;
 
-    JavaVersion(String fullVersion,String name) {
+    JavaVersion(String name) {
         this.name = name;
-        this.fullVersion=fullVersion;
     }
 
     public String getName() {
@@ -19,11 +17,4 @@ public enum JavaVersion {
         this.name = name;
     }
 
-    public String getFullVersion() {
-        return fullVersion;
-    }
-
-    public void setFullVersion(String fullVersion) {
-        this.fullVersion = fullVersion;
-    }
 }
