53f906c97f2ace1573abed3adc8bc0206309ac65
yaelna <nahon.yael@gmail.com>
2018-11-18 15:03:09 +0200

    added openjdk 11 to javaVersion enum

--- newman-common/src/main/java/com/gigaspaces/newman/beans/JavaVersion.java
+++ newman-common/src/main/java/com/gigaspaces/newman/beans/JavaVersion.java
@@ -1,7 +1,7 @@
 package com.gigaspaces.newman.beans;
 
 public enum JavaVersion {
-    ORACLE_7_21("oracle_7_21"), ORACLE_8_45("oracle_8_45"), ORACLE_9_0_4("oracle_9_0_4");
+    ORACLE_7_21("oracle_7_21"), ORACLE_8_45("oracle_8_45"), ORACLE_9_0_4("oracle_9_0_4"), OPENJDK_11_0_1("openjdk_11_0_1");
 
     private String name;
 
