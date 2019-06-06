0474a77ff6ddf1e63305bc05d569e4978202498a
yohanakh <yohana.khoury@gmail.com>
2019-06-06 18:33:20 +0300

    added --rm in server-run.sh

--- docker/server-run.sh
+++ docker/server-run.sh
@@ -11,4 +11,4 @@ if [[ -n "$1" ]]; then
 fi
 
 
-docker run ${MODE} --link mongo-server -v "${DIRNAME}/..":"/newman" --name newman-server --user $(id -u) -p 8443:8443 newman "/newman/newman-server/bin/newman-server.sh"
+docker run ${MODE} --link mongo-server --rm -v "${DIRNAME}/..":"/newman" --name newman-server --user $(id -u) -p 8443:8443 newman "/newman/newman-server/bin/newman-server.sh"
