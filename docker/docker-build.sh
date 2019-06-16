282200c583b1449611a6c410a11c942b28f06f53
yohanakh <yohana.khoury@gmail.com>
2019-06-16 21:41:37 +0300

    moved pull mongo to docker-build

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -2,3 +2,5 @@
 DIRNAME=`cd $(dirname ${BASH_SOURCE[0]}) && pwd`
 
 docker build --build-arg user=$USER --build-arg uid=$(id -u) --build-arg gid=$(id -g) -t newman ${DIRNAME}
+
+docker pull mongo
--- docker/server-build.sh
+++ docker/server-build.sh
@@ -6,4 +6,3 @@ if [[ ! -e "${M2}" ]]; then
     mkdir $HOME/.m2
 fi
 docker run -v ${M2}:${M2} -v "${DIRNAME}/..":"/newman" --user $(id -u) newman "/newman/newman-server/bin/build.sh"
-docker pull mongo
