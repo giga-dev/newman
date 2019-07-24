2a46ee88909d002702670827ee9c866691d2e276
Yohana Khoury <yohana@yohanakhoury.com>
2019-07-24 19:18:40 +0300

    Use mongo version 4.0.10

--- newman-server/mongo-restart.sh
+++ newman-server/mongo-restart.sh
@@ -11,4 +11,4 @@ fi
 
 docker kill mongo-server
 docker rm mongo-server
-docker run --hostname=mongo-server --name mongo-server -p 27017:27017 -v `pwd`/data/db:/data/db --name mongo-server ${MODE} mongo --nounixsocket
+docker run --hostname=mongo-server --name mongo-server -p 27017:27017 -v `pwd`/data/db:/data/db --name mongo-server ${MODE} mongo:4.0.10 --nounixsocket
