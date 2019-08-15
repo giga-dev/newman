5ecd0df41866c6ec8a45ae57a0b40b4b0921f485
Efrat Kimchi <efrat.kimchi@gigaspaces.com>
2019-08-15 10:38:57 +0300

    fix bug of agent groups- onclick on the screen close the agent group dropdown

--- newman-server/web/elm/src/Pages/SubmitNewJob.elm
+++ newman-server/web/elm/src/Pages/SubmitNewJob.elm
@@ -265,10 +265,10 @@ view model =
                 ]
             , br [] []
             , div [ style [ ( "width", "500px" ) ] ]
-                            [ text "Select agent groups:"
-                            , Multiselect.view model.selectedAgentGroups |> Html.map MultiSelectAgentGroupsMsg
-                            ]
-                        , br [] []
+                [ text "Select agent groups:"
+                , Multiselect.view model.selectedAgentGroups |> Html.map MultiSelectAgentGroupsMsg
+                ]
+                , br [] []
             , let
                 toOption data =
                     Select.item [ value data.id, selected <| model.selectedConfig == data.id ] [ text <| data.name ]
@@ -373,7 +373,11 @@ buildsAndSuitesDecoder =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.map MultiSelectMsg <| Multiselect.subscriptions model.selectedSuites
+    Sub.batch
+        [ Sub.map MultiSelectMsg <| Multiselect.subscriptions model.selectedSuites
+        , Sub.map MultiSelectAgentGroupsMsg <| Multiselect.subscriptions model.selectedAgentGroups
+        ]
+
 
 
 handleEvent : WebSocket.Event -> Cmd Msg
