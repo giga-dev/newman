00d442049c61ca1693fe01f699ca95bec173755c
mavigdor <moran@gigaspaces.com>
2023-05-07 15:04:19 +0300

    update ini file and remove insight-edge suites from nightly build

--- ansible/roles/submitter_deploy/files/submitter-env.sh
+++ ansible/roles/submitter_deploy/files/submitter-env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-export BRANCH_FILE_PATH="/home/xap/docker/branch_list.txt"
-export NEWMAN_HOST=xap-newman
+export BRANCH_FILE_PATH="/data/newman/testing-grid/bin/branch_list.txt"
+export NEWMAN_HOST=localhost
 export NEWMAN_PORT=8443
 export NEWMAN_USER_NAME=root
 export NEWMAN_PASSWORD=root
-export NEWMAN_SUITES_FILE_LOCATION="/home/xap/testing-grid/bin/submitter-env.ini"
+export NEWMAN_SUITES_FILE_LOCATION="/data/newman/testing-grid/bin/submitter-env.ini"
