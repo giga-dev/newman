6cca58e4a7334686f06e35dedd9d2a671c8c6fb3
Ariel Shatil <ariel.shatil@gigaspaces.com>
2024-07-09 12:27:06 +0300

    Setting default JavaVersion to 17

--- newman-common/src/main/java/com/gigaspaces/newman/NewmanConsts.java
+++ newman-common/src/main/java/com/gigaspaces/newman/NewmanConsts.java
@@ -5,5 +5,5 @@ import com.gigaspaces.newman.beans.JavaVersion;
 public class NewmanConsts {
 
     public static String DEFAULT_CONFIG_NAME = "Default Configuration";
-    public static JavaVersion DEFAULT_JAVA_VERSION = JavaVersion.ORACLE_8_45;
+    public static JavaVersion DEFAULT_JAVA_VERSION = JavaVersion.OPENJDK_17_0_1;
 }
