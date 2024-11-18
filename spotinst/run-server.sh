125fc059b57fe314fc29b9d9b8a9f087ed8e9ee2
Oleksii Vedernikov <oleksii.vedernikov@gigaspaces.com>
2024-11-18 23:43:56 +0100

    PIC-5324: Newman new UI - minor things updated

--- spotinst/run-server.sh
+++ spotinst/run-server.sh
@@ -4,8 +4,8 @@ DIRNAME=$(dirname ${BASH_SOURCE[0]})
 
 cd ${DIRNAME}/../newman-server/
 
-echo "running elm make"
-./elm-make.sh
+#echo "running elm make"
+#./elm-make.sh
 
 echo "running mongo-restart.sh"
 ./mongo-restart.sh
